93019cf96ee1e2bf06be71326a08e4db527fb94d
Ovidio R. Calvet <calveto@gmail.com>
2018-07-20 23:51:58 -0400

    Show todos from todo list

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  @Input()
+  item: any;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('ITEM', this.item);
   }
 
 }
